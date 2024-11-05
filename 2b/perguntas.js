e7df8484c2a7c42fd2b1bf72d2b51348d80646cc
kauan-moisés <132941077+kauanjogador@users.noreply.github.com>
2024-11-05 09:58:12 -0300

    Add files via upload

---
+++ 2b/perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+   'anime',
+  ' quem é o protagonista do anime Overlord?',
+    'ainz',
+)
+
+criaCartao(
+   'Geografia',
+    'qual continente fica o brasil?',
+    'america do sul',
+)
+
+criaCartao(
+   'jogos',
+    pergunta,
+    resposta,
+)
